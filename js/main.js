d2f0f1660d664007b975cdbab33773f8709d4f1a
Nadiia Kozhushko <kozhushko51@gmail.com>
2019-12-02 12:47:38 +0200

    check little bug

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
- $('.timer').countdown('2020/01/01', function(event) {
+
+$(function(){
+     $('.timer').countdown('2020/01/01', function(event) {
     $(this).html(event.strftime(""
     + '<div class="date"><span>%D</span></div><div class="semicolom"><div></div><div></div></div>'
     + '<div class="date"><span>%H</span></div><div class="semicolom"><div></div><div></div></div>'
@@ -6,3 +8,9 @@
     + '<div class="date"><span>%S</span></div>'
     ));
   });
+    
+    $('.reviews-slider').slick({
+        infinite: true,
+        dots:true,
+    });
+});
